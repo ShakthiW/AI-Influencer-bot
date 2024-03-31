848ce045a9c75e3d5677f2e587161c38f84177fe
Shakthi Warnakulasuriya <shakthi.20221274@iit.ac.lk>
2024-04-01 03:19:14 +0530

    Add image generation and upload functionality

--- generate_img.py
+++ generate_img.py
@@ -2,26 +2,28 @@ import json
 import requests
 import os
 
-API_KEY = os.getenv('API_KEY')
-
-headers = {"Authorization": f"Bearer {API_KEY}"}
-
-url = "https://api.edenai.run/v2/image/generation"
-payload = {
-    "providers": "replicate",
-    "text": "A beautiful anime girl with long hair and a sword.",
-    "response_as_dict": True,
-    "attributes_as_list": False,
-    "resolution": "1024x1024",
-    "num_images": 1,
-    "fallback_providers": "openai",
-}
-
-
-
-response = requests.post(url, json=payload, headers=headers)
-result = json.loads(response.text)
 # print(result['replicate']['items'])
+
+def generate_image(API_KEY, text, image_count=1):
+    headers = {"Authorization": f"Bearer {API_KEY}"}
+
+    url = "https://api.edenai.run/v2/image/generation"
+    payload = {
+        "providers": "replicate",
+        "text": text,
+        "response_as_dict": True,
+        "attributes_as_list": False,
+        "resolution": "1024x1024",
+        "num_images": image_count,
+        "fallback_providers": "openai",
+    }
+
+
+
+    response = requests.post(url, json=payload, headers=headers)
+    result = json.loads(response.text)
+    # print(result)
+    return result['replicate']['items'][0]['image_resource_url']
 
 
 # image_count = random.randint(1, 1000)
--- upload.py
+++ upload.py
@@ -1,17 +1,22 @@
 from instabot import Bot
 import os
+from generate_img import generate_image
+from download_img import download_image
 
 insta_pw = os.getenv("INSTA_PW")
 insta_user = os.getenv("INSTA_USER")
+API_KEY = os.getenv('API_KEY')
+
+image_count = len(os.listdir("Images/"))
 
 bot = Bot()
-bot.login(username=insta_user, password=insta_pw)
+bot.login(username="aiarchitectural_images", password="bcshakthi123")
+
+prompt = "A beautiful anime girl with long hair and a sword"
+
+image_url = generate_image(API_KEY, text=prompt)
+download_image(image_url, "Images/", f"image{image_count+1}")
 
 
-
-
-
-
-
-bot.upload_photo("yoda.jpg", caption="biscuit eating baby")
+bot.upload_photo("Images/image690.jpg", caption=f"prompt: {prompt}")
 
